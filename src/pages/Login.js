4da86fdfe4421bb67efdb3e51b0e891eff42fdda
Aakash-Rajput22 <ar8559605@gmail.com>
2026-06-26 10:53:46 +0530

    Added useState in Login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,42 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/login.css";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
     <div className="login-container">
       <div className="login-box">
         <h2>Login</h2>
 
-        <input type="email" placeholder="Enter Email" />
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <input type="password" placeholder="Enter Password" />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <button>Login</button>
+        <button onClick={handleLogin}>
+          Login
+        </button>
 
         <p>
-          Don't have an account? <a href="/signup">Signup</a>
+          Don't have an account?
+          <Link to="/signup"> Signup</Link>
         </p>
       </div>
     </div>
