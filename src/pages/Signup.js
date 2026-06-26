7f2d22915503e67f4ef5b6b7d5a68b083fedca3c
Aakash-Rajput22 <ar8559605@gmail.com>
2026-06-26 10:43:30 +0530

    Added Signup UI and reusable components

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,45 @@
+import { Link } from "react-router-dom";
+import InputField from "../components/InputField";
+import Button from "../components/Button";
+import "../styles/signup.css";
+
 function Signup() {
-  return <h1>Signup Page</h1>;
+  return (
+    <div className="signup-container">
+      <div className="signup-box">
+
+        <h2>Create Account</h2>
+
+        <InputField
+          type="text"
+          placeholder="Full Name"
+        />
+
+        <InputField
+          type="email"
+          placeholder="Email"
+        />
+
+        <InputField
+          type="password"
+          placeholder="Password"
+        />
+
+        <InputField
+          type="password"
+          placeholder="Confirm Password"
+        />
+
+        <Button text="Signup" />
+
+        <p>
+          Already have an account?
+          <Link to="/login"> Login</Link>
+        </p>
+
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
