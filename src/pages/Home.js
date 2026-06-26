a6a22cea78d0023e92422b1a0e0a226ff3a8add9
Aakash-Rajput22 <ar8559605@gmail.com>
2026-06-26 15:24:58 +0530

    Added professional home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+import "../styles/home.css";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="home-container">
+      <div className="home-box">
+        <h1>React Firebase Authentication</h1>
+
+        <p>
+          Welcome to the Mini Project built using React.js and Firebase.
+        </p>
+
+        <div className="home-buttons">
+          <Link to="/login">
+            <button className="home-btn">Login</button>
+          </Link>
+
+          <Link to="/signup">
+            <button className="home-btn signup-btn">
+              Create Account
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
