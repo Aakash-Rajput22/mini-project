d8431d6825eea7cf2ad726271edaf8bfd69f965a
Aakash-Rajput22 <ar8559605@gmail.com>
2026-06-26 10:26:19 +0530

    Added Login UI

---
+++ src/pages/Login.js
@@ -0,0 +1,22 @@
+import "../styles/login.css";
+function Login() {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+
+        <input type="email" placeholder="Enter Email" />
+
+        <input type="password" placeholder="Enter Password" />
+
+        <button>Login</button>
+
+        <p>
+          Don't have an account? <a href="/signup">Signup</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
